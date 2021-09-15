58f418ad5775c759ada4c61e3985916e4c69d94d
VernonLovegreen <90652219+VernonLovegreen@users.noreply.github.com>
2021-09-15 16:29:06 -0500

    Update pets.js

--- forms/pets/pets.js
+++ forms/pets/pets.js
@@ -1,5 +1,6 @@
-petNames = ["Darcy", "Jack", "Jerry", "Marty", "Vito", "Vinny", "Riley", "Saddie", "Marge", "Sam"]
-withSal = ["Darcy", "Jack", "Jerry", "Marty", "Vito", "Vinny", "Riley", "Saddie", "Marge", "Sam", "Sal"]
+let petNames = ["Darcy", "Jack", "Jerry", "Marty", "Vito", "Vinny", "Riley", "Saddie", "Marge", "Sam"]
+let withSal = petNames 
+withSal.push("Sal")
  console.log(withSal)
 // console.log(`The third pet is named ${petNames[2]}`)
 console.log(withSal.length)
